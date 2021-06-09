306d8fcff2f2d6cb5a7a4a4ada42936aafce85cd
fmoreira <fabianomoreira.prof@gmail.com>
2021-06-09 20:13:52 -0300

    inicio

--- lojaPlus/src/app/app-routing.module.ts
+++ lojaPlus/src/app/app-routing.module.ts
@@ -25,6 +25,14 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'produto-add',
+    loadChildren: () => import('./pages/produto-add/produto-add.module').then( m => m.ProdutoAddPageModule)
+  },
+  {
+    path: 'produto-list',
+    loadChildren: () => import('./pages/produto-list/produto-list.module').then( m => m.ProdutoListPageModule)
+  },
 
  
 
---
+++ lojaPlus/src/app/pages/produto-add/produto-add.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-produto-add',
+  templateUrl: './produto-add.page.html',
+  styleUrls: ['./produto-add.page.scss'],
+})
+export class ProdutoAddPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
