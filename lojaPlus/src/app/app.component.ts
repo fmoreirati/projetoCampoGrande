4b9e355ba0fe7045fd39bb9aaef7893de2290eee
fmoreira <fabianomoreira.prof@gmail.com>
2021-06-11 21:29:08 -0300

    produtos

--- lojaPlus/src/app/app.component.ts
+++ lojaPlus/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +14,9 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    protected auth: AngularFireAuth
+
   ) {
     this.initializeApp();
   }
